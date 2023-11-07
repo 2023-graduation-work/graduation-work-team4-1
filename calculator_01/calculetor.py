af818c5eb6544018c09a03b4c39f28ae1338c171
こうへい <k.sawada.sys22@morijyobi.ac.jp>
2023-11-07 11:20:52 +0900

    電卓の作成

--- calculator_01/calculetor.py
+++ calculator_01/calculetor.py
@@ -2,12 +2,32 @@ import tkinter as tk
 import math
 from tkinter import ttk
 
+memory = 0
+
+def add_to_memory():
+    global memory
+    current_value = float(entry.get())
+    memory += current_value
+    entry.delete(0, tk.END)
+
+def subtract_from_memory():
+    global memory
+    current_value = float(entry.get())
+    memory -= current_value
+    entry.delete(0, tk.END)
+
+def display_memory():
+    entry.delete(0, tk.END)
+    entry.insert(0, str(memory))
+
 def button_click(number):
     current = entry.get()
     entry.delete(0, tk.END)
     entry.insert(0, current + str(number))
 
 def clear():
+    global memory  # メモリーをクリア
+    memory = 0
     entry.delete(0, tk.END)
 
 def calculate():
@@ -16,7 +36,7 @@ def calculate():
         result = eval(expression)
         entry.delete(0, tk.END)
         entry.insert(0, result)
-        history_list.insert(tk.END, f"{expression} = {result}")
+        history_list.insert(tk.END, f"{expression} = {math.degrees(result)}°")
     except Exception as e:
         entry.delete(0, tk.END)
         entry.insert(0, "エラー")
@@ -25,13 +45,15 @@ def calculate():
 def calculate_function(func):
     expression = entry.get()
     try:
+        value = float(expression)
+        value_rad = math.radians(value)
         if func == 'sqrt':
-            result = math.sqrt(eval(expression))
+            result = math.sqrt(value_rad)
         else:
-            result = getattr(math, func)(eval(expression))
+            result = getattr(math, func)(value_rad)
         entry.delete(0, tk.END)
         entry.insert(0, result)
-        history_list.insert(tk.END, f"{func}({expression}) = {result}")
+        history_list.insert(tk.END, f"{func}({math.degrees(value_rad)}°) = {math.degrees(result)}°")
     except Exception as e:
         entry.delete(0, tk.END)
         entry.insert(0, "エラー")
@@ -43,8 +65,26 @@ def exit_app():
 def convert_units(history_list):
     input_value = float(entry_value.get())
     input_unit = unit_var.get()
-
-    if input_unit == "時間から分":
+    
+    if input_unit == "日から時間":
+        result = input_value * 24
+        result_label.config(text=f"{input_value} 日は {result} 時間です")
+    elif input_unit == "時間から日":
+        result = input_value / 24
+        result_label.config(text=f"{input_value} 時間は {result} 日です")
+    elif input_unit == "日から分":
+        result = input_value * 1440
+        result_label.config(text=f"{input_value} 日は {result} 分です")
+    elif input_unit == "分から日":
+        result = input_value / 1440
+        result_label.config(text=f"{input_value} 分は {result} 日です")
+    elif input_unit == "日から秒":
+        result = input_value * 86400
+        result_label.config(text=f"{input_value} 日は {result} 秒です")
+    elif input_unit == "秒から日":
+        result = input_value / 86400
+        result_label.config(text=f"{input_value} 秒は {result} 日です")
+    elif input_unit == "時間から分":
         result = input_value * 60
         result_label.config(text=f"{input_value} 時間は {result} 分です")
     elif input_unit == "分から時間":
@@ -53,10 +93,17 @@ def convert_units(history_list):
     elif input_unit == "時間から秒":
         result = input_value * 3600
         result_label.config(text=f"{input_value} 時間は {result} 秒です")
+    elif input_unit == "秒から時間":
+        result = input_value / 3600
+        result_label.config(text=f"{input_value} 秒は {result} 時間です")
+    elif input_unit == "分から秒":
+        result = input_value * 60
+        result_label.config(text=f"{input_value} 分は {result} 秒です")
     elif input_unit == "秒から分":
         result = input_value / 60
         result_label.config(text=f"{input_value} 秒は {result} 分です")
     history_list.insert(tk.END, result_label.cget("text"))
+
 
 root = tk.Tk()
 root.title("アプリ")
@@ -93,7 +140,6 @@ buttons = [
     ("4", "white"), ("5", "white"), ("6", "white"), ("*", "white"),
     ("1", "white"), ("2", "white"), ("3", "white"), ("-", "white"),
     ("0", "white"), (".", "white"), ("=", "white"), ("+", "white"),
-    ("sqrt", "white"), ("sin", "white"), ("cos", "white"), ("tan", "white"),
 ]
 
 button_frame = tk.Frame(frame_calculator)
@@ -101,6 +147,28 @@ button_frame.pack()
 
 row_val = 0
 col_val = 0
+
+add_memory_button = tk.Button(button_frame, text="M+", command=add_to_memory, padx=5, pady=5, width=btn_width, height=btn_height, bg="white", font=('Helvetica', 14))
+add_memory_button.grid(row=row_val, column=col_val, padx=2, pady=2)
+
+col_val += 1
+
+subtract_memory_button = tk.Button(button_frame, text="M-", command=subtract_from_memory, padx=5, pady=5, width=btn_width, height=btn_height, bg="white", font=('Helvetica', 14))
+subtract_memory_button.grid(row=row_val, column=col_val, padx=2, pady=2)
+
+col_val += 1
+
+display_memory_button = tk.Button(button_frame, text="M", command=display_memory, padx=5, pady=5, width=btn_width, height=btn_height, bg="white", font=('Helvetica', 14))
+display_memory_button.grid(row=row_val, column=col_val, padx=2, pady=2)
+
+col_val += 1
+
+clear_button = tk.Button(button_frame, text="C", command=clear, padx=5, pady=5, width=btn_width, height=btn_height, bg="white", font=('Helvetica', 14))
+clear_button.grid(row=row_val, column=col_val, padx=2, pady=2)
+
+row_val += 1
+col_val = 0
+
 for label, color in buttons:
     if label in ["sqrt", "sin", "cos", "tan"]:
         tk.Button(button_frame, text=label, padx=5, pady=5, width=btn_width, height=btn_height, command=lambda b=label: calculate_function(b), bg=color, font=('Helvetica', 14)).grid(row=row_val, column=col_val, padx=2, pady=2)
@@ -112,21 +180,18 @@ for label, color in buttons:
         col_val = 0
         row_val += 1
 
-clear_button = tk.Button(frame_calculator, text="クリア", padx=5, pady=5, width=btn_width * 2, height=btn_height, command=clear, bg="white", font=('Helvetica', 16))
-clear_button.pack()
-
 history_label = tk.Label(frame_history, text="履歴", bg="black", fg="white", font=('Helvetica', 16))
 history_label.pack()
 
 history_list = tk.Listbox(frame_history, bg="white", font=('Helvetica', 14))
 history_list.pack(fill="both", expand=True)
 
-instruction_label = tk.Label(frame_time_converter,font=('Helvetica', 14))
+instruction_label = tk.Label(frame_time_converter, font=('Helvetica', 14))
 instruction_label.pack()
 
 unit_var = tk.StringVar()
 unit_var.set("時間から分")
-unit_option_menu = tk.OptionMenu(frame_time_converter, unit_var, "時間から分", "分から時間", "時間から秒", "秒から分")
+unit_option_menu = tk.OptionMenu(frame_time_converter, unit_var,"日から時間", "時間から日", "日から分", "分から日", "日から秒", "秒から日", "時間から分", "分から時間", "時間から秒", "秒から時間", "分から秒", "秒から分")
 unit_option_menu.pack()
 
 entry_value = tk.Entry(frame_time_converter, font=('Helvetica', 16))
